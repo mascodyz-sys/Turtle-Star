3ba44e98654e1cd61114f063f442f699e4d94502
mascodyz-sys <mascodyz@gmail.com>
2026-02-17 16:31:06 +0530

    Update main.py
    Code of a star created with Turtle in python

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 
+import turtle
+# Create screen
+screen = turtle.Screen()
+screen.setup(width=400, height=300)
+screen.bgcolor("white")
+screen.title("Turtle Start Example")
+# Create turtle
+t = turtle.Turtle()
+t.pencolor("blue")
+t.pensize(2)
+# Draw a square
+for _ in range(4):
+   t.forward(100)
+   t.right(90)
+screen.exitonclick()
